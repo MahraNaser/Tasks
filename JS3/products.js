83416c8c6a5f6e33a779115dc0f21fe76dd17401
MahraNaser <85400191+MahraNaser@users.noreply.github.com>
2022-12-12 12:17:40 +0400

    Add files via upload

---
+++ JS3/products.js
@@ -0,0 +1,56 @@
+const products = [
+    {
+      id: 0,
+      name: "Flowered Dress",
+      price: 30,
+      instock: 100,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "111.jpg",
+    },
+    {
+      id: 1,
+      name: "Hoodie",
+      price: 40,
+      instock: 43,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "IMG_0664.jpg",
+    },
+    {
+      id: 2,
+      name: "Jacket",
+      price: 15,
+      instock: 10,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "IMG_0667.jpg",
+    },
+    {
+      id: 3,
+      name: "Mid Skirt",
+      price: 20,
+      instock: 5,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "IMG_0666.jpg",
+    },
+    {
+      id: 4,
+      name: "Boots",
+      price: 25,
+      instock: 4,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "IMG_B4372538FCE9-1.jpeg",
+    },
+    {
+      id: 5,
+      name: "Tote Bag",
+      price: 30,
+      instock: 40,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "IMG_0789.jpg",
+    },
+  ];
